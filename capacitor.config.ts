ecd3f48b79b4b3b71c6fc90fbea035452de69613
kit_lee <leedg7120@naver.com>
2025-09-25 11:30:21 +0900

    feat: update network security config and capacitor configuration for improved domain handling and navigation

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.celery.salespt',
   appName: 'Celery',
-  webDir: 'build'
+  webDir: 'build',
+  server: {
+    allowNavigation: ['*'],
+    androidScheme: 'http'
+  }
 };
 
 export default config;
